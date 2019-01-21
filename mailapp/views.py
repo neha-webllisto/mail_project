e0f0ad1cc133bbff2f2af6a653af7439d4d082eb
Mansi <mansi@webllisto.com>
2019-01-21 12:44:42 +0530

    mail

---
+++ mailapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import HttpResponse
+from django.conf import settings
+from mailproject import settings
+
+# Create your views here.
+
+def mail(request):
+	subject = 'Greetings'
+	msg = 'Hello Neha'
+	to = 'neha.webllisto@gmail.com'
+	res = send_mail(subject, msg, settings.EMAIL_HOST_USER, [to])
+
+	if res == 1:
+		msg = 'Mail sent successfully'
+
+	else:
+		msg = 'Mail could not send'
+
+	return HttpResponse(msg)
